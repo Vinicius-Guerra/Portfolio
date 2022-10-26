11b639601f23e70f52822096d9f588260d381ac7
Vinicius-Guerra <vini.fms@hotmail.com>
2022-10-25 22:28:04 -0200

    function incio

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function aparecerTexto() {
+    var mudarTexto = document.querySelector('.descricao_texto');
+    mudarTexto.innerHTML = "Html";
+}
+function reset() {
+    var mudarTexto = document.querySelector('.descricao_texto');
+    mudarTexto.innerHTML = "/* Mantenha o mouse em cima de alguma tecnologia ao lado<br> para ler a descricao. */"
+}
