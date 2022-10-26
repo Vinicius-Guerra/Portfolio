598a6f39be729d90ec2f5fce3d94edd6775d2981
Vinicius-Guerra <vini.fms@hotmail.com>
2022-10-26 17:46:07 -0200

    update

--- script/main.js
+++ script/main.js
@@ -1,8 +1,14 @@
-function aparecerTexto() {
+function aparecerTextoHtml() {
     var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "Html";
+    mudarTexto.innerHTML = "HTML5, trata-se de uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web. Realizo toda formatação de Acessibilidade web com intuito de tornar seu front-end inclusivo, além das práticas SEO para otimização de sites.";
 }
-function reset() {
+
+function aparecerTextoCss() {
     var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "/* Mantenha o mouse em cima de alguma tecnologia ao lado<br> para ler a descricao. */"
+    mudarTexto.innerHTML = "CSS3, amplamente utilizada com HTML, representa diversas possibilidades para formatação, o CSS ajuda a estilizar, editar, alinhar, remover e trabalhar no espaço entre elementos de uma página."
 }
+
+/*function reset() {
+    var mudarTexto = document.querySelector('.descricao_texto');
+    mudarTexto.innerHTML = "/* Mantenha o mouse em cima de alguma tecnologia ao lado<br> para ler a descricao.
+} */ 
