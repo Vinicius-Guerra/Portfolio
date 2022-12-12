25e0b3464f820981b0aa731c5f452b984f6702f0
Vinicius-Guerra <vini.fms@hotmail.com>
2022-12-12 17:13:46 -0200

    Error

--- script/main.js
+++ script/main.js
@@ -1,54 +1,15 @@
-function aparecerTextoHtml() {
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "HTML5, trata-se de uma linguagem de marcação de hipertexto para apresentar e estruturar o conteúdo na web. Realizo toda formatação de Acessibilidade web com intuito de tornar seu front-end inclusivo, além das práticas SEO para otimização de sites.";
+var txtPadrao = document.querySelector('.inicial');
+var mostrarht = document.querySelector('.oculto_html');
+const listaDeTeclas = document.querySelectorAll('.teclas');
+
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[c];
+    tecla.onclick = exibirTexto();
 }
 
-function aparecerTextoCss() {
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "CSS3, amplamente utilizada com HTML, representa diversas possibilidades para formatação, o CSS ajuda a estilizar, editar, alinhar, remover e trabalhar no espaço entre elementos de uma página web desktop e mobile.";
+function exibirTexto () {
+    txtPadrao.style.display = "none";
+    if (listaDeTeclas[0]) {
+        mostrarht.style.display = "flex";
+    }   
 }
-
-function aparecerTextoAngular(){
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "Angular, trata-se de um framework de código aberto usado para construir aplicativos para web baseados em uma única página dinâmica. O AngularJS combina JavaScript e HTML.";
-}
-
-function aparecerTextoJs(){
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "JavaScript, é uma linguagem de programação de alto nivel, usada para criar páginas interativas, as funções do JavaScript podem melhorar a experiência do usuário durante sua navegação. O JavaScript pode ser utilizado para Front end e Back end."
-}
-
-function aparecerTextoBoots(){
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "Bootstrap, é um framework front-end que fornece estruturas de CSS para criação de sites e aplicações responsivas de forma rápida e simples. Além disso, pode lidar com sites de desktop e páginas de dispositivos móveis da mesma forma.";
-}
-
-function aparecerTextoGit(){
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "Git, é um repositório de controle de versão de arquivos, utilizado para controlar versões de códigos-fonte e desenvolvimento colaborativo.";
-}
-
-function aparecerTextoGitHub(){
-    var mudarTexto = document.querySelector('.descricao_texto')
-    mudarTexto.innerHTML = "GitHub, é o maior repositório de dados compartilhado do mundo. É uma plataforma majoritariamente usada por desenvolvedores, pois permite uma hospedagem prática de código-fonte e arquivos em nuvem."
-}
-
-function aparecerTextoNode(){
-    var mudarTexto = document.querySelector('.descricao_texto')
-    mudarTexto.innerHTML = "Node.js, se caracteriza como um ambiente de execução JavaScript. Com ele, o usuário pode criar aplicações sem depender do browser para isso. Com alta capacidade de escalabilidade, boa flexibilidade, arquitetura e baixo custo, torna-se uma ótima opção para programação."
-}
-
-function aparecerTextoMySql(){
-    var mudarTexto = document.querySelector('.descricao_texto')
-    mudarTexto.innerHTML = "MySQL, consiste num Sistema Gerenciador de Banco de Dados (SGBD), cujo trabalha com a linguagem SQL. Dessa forma, para interagir com o banco de dados MySQL, é preciso conhecer os comandos SQL."
-}
-
-function aparecerTextoJava(){
-    var mudarTexto = document.querySelector('.descricao_texto')
-    mudarTexto.innerHTML = "Java, é uma linguagem de programaçao baseada em classes e orientada a objetos, com foco em segurança, portabilidade e alta performance."
-}
-
-/*function reset() {
-    var mudarTexto = document.querySelector('.descricao_texto');
-    mudarTexto.innerHTML = "/* Mantenha o mouse em cima de alguma tecnologia ao lado<br> para ler a descricao.
-} */ 
